819ea53402d41cf4d265181c2888974e7f3068cf
Raphael H Ribas <raphael.ribas@gmail.com>
2019-12-17 22:54:05 -0500

    larger network is so good

--- model.py
+++ model.py
@@ -9,29 +9,51 @@ from tensorflow.keras.layers import (
 from tensorflow.keras import Model
 
 
+class Block(Model):
+    def __init__(self, name, filters, layers):
+        super().__init__(name=name)
+
+        self._layers = []
+
+        for i in range(layers):
+            self._layers.append(
+                Conv2D(
+                    filters,
+                    3,
+                    padding="same",
+                    activation="relu",
+                    name=f"{name}/conv_{i}",
+                )
+            )
+        self._layers.append(MaxPooling2D(name=f"{name}/3"))
+
+    def call(self, x, training=False):
+        for layer in self._layers:
+            x = layer(x, training=training)
+        return x
+
+
 class AgentModel(Model):
     def __init__(self, name, board_shape):
         super().__init__(name=name)
 
         self._layers = [
-            Reshape(list(board_shape) + [1], input_shape=board_shape, name=f"{name}/1"),
-            Conv2D(32, 3, padding="same", activation="relu", name=f"{name}/3"),
-            Conv2D(32, 3, padding="same", activation="relu", name=f"{name}/4"),
-            MaxPooling2D(name=f"{name}/5"),
-            Dropout(0.5, name=f"{name}/6"),
-            Conv2D(64, 3, padding="same", activation="relu", name=f"{name}/7"),
-            Conv2D(64, 3, padding="same", activation="relu", name=f"{name}/8"),
-            MaxPooling2D(name=f"{name}/9"),
-            Dropout(0.5, name=f"{name}/10"),
-            Conv2D(128, 3, padding="same", activation="relu", name=f"{name}/11"),
-            Conv2D(128, 3, padding="same", activation="relu", name=f"{name}/12"),
-            MaxPooling2D(name=f"{name}/13"),
-            Dropout(0.5, name=f"{name}/14"),
-            Flatten(name=f"{name}/15"),
-            Dense(1024, activation="relu", name=f"{name}/16"),
-            Dense(1024, activation="relu", name=f"{name}/17"),
-            Dense(1, activation="linear", name=f"{name}/18"),
+            Reshape(
+                list(board_shape) + [1], input_shape=board_shape, name=f"{name}/reshape"
+            )
         ]
+
+        for i, size in enumerate([32, 64, 128]):
+            self._layers.append(Block(name=f"{name}/block_{i}", filters=size, layers=4))
+
+        self._layers.extend(
+            [
+                Flatten(name=f"{name}/15"),
+                Dense(1024, activation="relu", name=f"{name}/dense_1"),
+                Dense(1024, activation="relu", name=f"{name}/dense_2"),
+                Dense(1, activation="linear", name=f"{name}/readout"),
+            ]
+        )
 
     def call(self, x, training=False):
         for layer in self._layers:
