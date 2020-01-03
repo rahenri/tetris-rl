1bc7bc51b65bbb6ac0670393aa2fe0d7c135842d
Raphael H Ribas <raphael.ribas@gmail.com>
2020-01-03 13:38:04 -0500

    Better plotting

--- plot.py
+++ plot.py
@@ -20,17 +20,54 @@ def main():
     )
     args = parser.parse_args()
     metric = "reward"
+    num_buckets = 200
 
     for experiment in args.experiments:
         filename = os.path.join(args.experiment_dir, experiment, "episodes.txt")
         episode_numbers = []
-        pieces = []
+        values = []
         with open(filename) as f:
             for line in f:
                 record = json.loads(line)
-                episode_numbers.append(record["episode"])
-                pieces.append(record[metric])
-        plt.plot(episode_numbers, pieces, label=experiment)
+                episode_numbers.append(int(record["episode"]))
+                values.append(float(record[metric]))
+        minx = np.min(episode_numbers)
+        maxx = np.max(episode_numbers)
+
+        bucket_max = [None] * num_buckets
+        bucket_min = [None] * num_buckets
+        bucket_sum = [0] * num_buckets
+        bucket_count = [0] * num_buckets
+
+        bucket_x = []
+        for i in range(num_buckets):
+            bucket_x.append(minx + (maxx - minx) * i / num_buckets)
+
+        for i, v in enumerate(values):
+            x = min(int((episode_numbers[i] - minx) / (maxx - minx) * num_buckets), num_buckets - 1)
+
+            acc = bucket_max[x]
+            if acc is not None:
+                acc = max(acc, v)
+            else:
+                acc = v
+            bucket_max[x] = acc
+
+            acc = bucket_min[x]
+            if acc is not None:
+                acc = min(acc, v)
+            else:
+                acc = v
+            bucket_min[x] = acc
+
+            bucket_sum[x] += v
+            bucket_count[x] += 1
+
+        bucket_mean = np.array(bucket_sum) / np.array(bucket_count)
+
+        plt.plot(bucket_x, bucket_max, label=f"{experiment} (max)")
+        plt.plot(bucket_x, bucket_min, label=f"{experiment} (min)")
+        plt.plot(bucket_x, bucket_mean, label=f"{experiment} (mean)")
     plt.legend()
 
     mplcursors.cursor(hover=True)
